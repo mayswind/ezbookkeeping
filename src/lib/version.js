545fec9086dcdde5608c142adfc422f89451e3f7
MaysWind <i@mayswind.net>
2020-11-20 21:31:05 +0800

    modify style

--- src/lib/version.js
+++ src/lib/version.js
@@ -1,8 +1,13 @@
 export default {
     getVersion: () => {
         let version = process.env.VERSION || 'unknown';
-        let commitHash = process.env.COMMIT_HASH || 'unknown';
-        return `${version}-${commitHash.substr(0, Math.min(10, commitHash.length))}`;
+        let commitHash = process.env.COMMIT_HASH;
+
+        if (commitHash) {
+            return `${version} (${commitHash.substr(0, Math.min(10, commitHash.length))})`
+        } else {
+            return version;
+        }
     },
     getBuildTime: () => {
         return process.env.BUILD_UNIXTIME;

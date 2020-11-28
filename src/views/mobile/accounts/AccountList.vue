c97848e61ea631e1fcef2a374968c24af045f2f2
MaysWind <i@mayswind.net>
2020-11-28 19:31:25 +0800

    disable pull to refresh in sort mode

--- src/views/mobile/accounts/AccountList.vue
+++ src/views/mobile/accounts/AccountList.vue
@@ -1,5 +1,5 @@
 <template>
-    <f7-page ptr @ptr:refresh="reload" @page:afterin="onPageAfterIn">
+    <f7-page :ptr="!sortable" @ptr:refresh="reload" @page:afterin="onPageAfterIn">
         <f7-navbar>
             <f7-nav-left :back-link="$t('Back')"></f7-nav-left>
             <f7-nav-title :title="$t('Account List')"></f7-nav-title>

4ed802b744d99c3ccdb62d14d55bf6b5d91b060a
MaysWind <i@mayswind.net>
2020-12-05 21:01:10 +0800

    modify file name

--- src/router/mobile.js
+++ src/router/mobile.js
@@ -8,8 +8,8 @@ import UnlockPage from '../views/mobile/Unlock.vue';
 import TransactionDetailPage from '../views/mobile/transactions/Detail.vue';
 import TransactionNewPage from '../views/mobile/transactions/New.vue';
 
-import AccountListPage from '../views/mobile/accounts/AccountList.vue';
-import AccountEditPage from '../views/mobile/accounts/AccountEdit.vue';
+import AccountListPage from '../views/mobile/accounts/List.vue';
+import AccountEditPage from '../views/mobile/accounts/Edit.vue';
 
 import StatisticsOverviewPage from '../views/mobile/statistics/Overview.vue';
 
@@ -22,10 +22,10 @@ import UserProfilePage from "../views/mobile/users/UserProfile.vue";
 import TwoFactorAuthPage from "../views/mobile/users/TwoFactorAuth.vue";
 import SessionListPage from "../views/mobile/users/SessionList.vue";
 
-import CategoryAllPage from "../views/mobile/categories/CategoryAll.vue";
-import CategoryListPage from "../views/mobile/categories/CategoryList.vue";
-import CategoryEditPage from "../views/mobile/categories/CategoryEdit.vue";
-import CategoryDefaultPage from "../views/mobile/categories/CategoryDefault.vue";
+import CategoryAllPage from "../views/mobile/categories/All.vue";
+import CategoryListPage from "../views/mobile/categories/List.vue";
+import CategoryEditPage from "../views/mobile/categories/Edit.vue";
+import CategoryDefaultPage from "../views/mobile/categories/Default.vue";
 
 function checkLogin(to, from, resolve, reject) {
     const router = this;

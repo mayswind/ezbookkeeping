848d3692a0dad76fbd6d6349f4014e09d6da1cca
MaysWind <i@mayswind.net>
2020-12-14 00:03:00 +0800

    remove used code

--- pkg/utils/slices.go
+++ pkg/utils/slices.go
@@ -1,23 +1,5 @@
 package utils
 
-func IsStringSliceEuqals(s1, s2 []string) bool {
-	if (s1 == nil) != (s2 == nil) {
-		return false
-	}
-
-	if len(s1) != len(s2) {
-		return false
-	}
-
-	for i := 0; i < len(s1); i++ {
-		if s1[i] != s2[i] {
-			return false
-		}
-	}
-
-	return true
-}
-
 func Int64SliceMinus(s1, s2 []int64) []int64 {
 	if s1 == nil {
 		return nil

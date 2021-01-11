c5385ce86b3a057061594cf4b1984bf66c804b6e
MaysWind <i@mayswind.net>
2021-01-12 00:45:03 +0800

    use git short revision style

--- src/lib/version.js
+++ src/lib/version.js
@@ -4,7 +4,7 @@ export default {
         let commitHash = process.env.COMMIT_HASH;
 
         if (commitHash) {
-            return `${version} (${commitHash.substr(0, Math.min(10, commitHash.length))})`
+            return `${version} (${commitHash.substr(0, Math.min(7, commitHash.length))})`
         } else {
             return version;
         }

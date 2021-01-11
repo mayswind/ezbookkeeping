1a33528710fbdaf5a56c47839613c578f92666b8
MaysWind <i@mayswind.net>
2021-01-12 00:27:21 +0800

    getting app version from package.json

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mode=$1;
-version=`grep "const LAB_VERSION" lab.go | awk -F '=' '{print $2}' | tr -d ' ' | tr -d '"'`;
+version=`grep '"version": ' package.json | awk -F ':' '{print $2}' | tr -d ' ' | tr -d ',' | tr -d '"'`;
 
 if [ "$mode" == "--snapshot" ]; then
   version="SNAPSHOT-"`date "+%Y%m%d"`;

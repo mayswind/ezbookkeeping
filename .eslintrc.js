5cdd5e6ed9c18e6110b4268eea005da55c5937e2
MaysWind <i@mayswind.net>
2023-04-21 00:51:15 +0800

    modify eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'root': true,
+    'env': {
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:vue/vue3-essential'
+    ],
+    'rules': {
+        "vue/multi-word-component-names": "off"
+    }
+}

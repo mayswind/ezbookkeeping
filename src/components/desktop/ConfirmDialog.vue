e3d1a476e2087a5a73a2bc8404a7ab3fa7904160
MaysWind <i@mayswind.net>
2023-06-24 23:59:20 +0800

    fix wrong watching parameter

--- src/components/desktop/ConfirmDialog.vue
+++ src/components/desktop/ConfirmDialog.vue
@@ -43,7 +43,7 @@ export default {
         }
     },
     watch: {
-        'show': function (newValue) {
+        'showState': function (newValue) {
             this.$emit('update:show', newValue);
         }
     },
@@ -67,7 +67,7 @@ export default {
             return new Promise((resolve, reject) => {
                 this.resolve = resolve;
                 this.reject = reject;
-            })
+            });
         },
         confirm() {
             if (this.resolve) {

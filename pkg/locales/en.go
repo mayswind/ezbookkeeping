caa83c04321ced721ee607fb3b1fef72381d8eeb
MaysWind <i@mayswind.net>
2023-08-28 00:16:16 +0800

    modify text

--- pkg/locales/en.go
+++ pkg/locales/en.go
@@ -4,8 +4,8 @@ var en = &LocaleTextItems{
 	ForgetPasswordMailTextItems: &ForgetPasswordMailTextItems{
 		Title:                     "Reset Your Password",
 		SalutationFormat:          "Hi %s,",
-		DescriptionAboveBtn:       "We recently received a request to reset your password. You can click the below link to reset your password.",
+		DescriptionAboveBtn:       "We recently received a request to reset your password. You can click the link below to reset your password.",
 		ResetPassword:             "Reset Password",
-		DescriptionBelowBtnFormat: "If you did not request to reset your password, please simply disregard this email. If you cannot click the above link, please copy the above url and paste it into your browser. The password reset link will be expired after %v minutes.",
+		DescriptionBelowBtnFormat: "If you did not request to reset your password, please simply disregard this email. If you cannot click the link above, please copy the above url and paste it into your browser. The password reset link will be expired after %v minutes.",
 	},
 }

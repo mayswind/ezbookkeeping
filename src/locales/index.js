fffe2a1ccb54aa92f58716c16b1148b05726bfdc
MaysWind <i@mayswind.net>
2024-12-21 22:13:00 +0800

    code refactor

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,6 +1,6 @@
-import en from './en.json'
-import vi from './vi.json'
-import zhHans from './zh_Hans.json'
+import en from './en.json';
+import vi from './vi.json';
+import zhHans from './zh_Hans.json';
 
 export const defaultLanguage = 'en';
 
@@ -12,17 +12,17 @@ export const allLanguages = {
         alternativeLanguageTag: 'en',
         content: en
     },
+    'vi': {
+        name: 'Vietnamese',
+        displayName: 'Tiếng Việt',
+        alternativeLanguageTag: 'vi-VN',
+        content: vi
+    },
     'zh-Hans': {
         name: 'Simplified Chinese',
         displayName: '简体中文',
         alternativeLanguageTag: 'zh-CN',
         aliases: ['zh-CHS', 'zh-CN', 'zh-SG'],
         content: zhHans
-    },
-    'vi': {
-        name: 'Vietnamese',
-        displayName: 'Tiếng Việt',
-        alternativeLanguageTag: 'vi-VN',
-        content: vi
     }
 };

ea90e97f922928e98bb924523c87b6c73e2754b5
MaysWind <i@mayswind.net>
2025-01-11 02:14:49 +0800

    update unit test

--- pkg/api/exchange_rates_test.go
+++ pkg/api/exchange_rates_test.go
@@ -23,7 +23,7 @@ func TestExchangeRatesApiLatestExchangeRateHandler_ReserveBankOfAustraliaDataSou
 
 	assert.Equal(t, "AUD", exchangeRateResponse.BaseCurrency)
 
-	supportedCurrencyCodes := []string{"CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "INR", "JPY", "KRW",
+	supportedCurrencyCodes := []string{"CAD", "CHF", "CNY", "EUR", "GBP", "HKD", "IDR", "INR", "JPY", "KRW",
 		"MYR", "NZD", "PHP", "SGD", "THB", "TWD", "USD", "VND"}
 
 	checkExchangeRatesHaveSpecifiedCurrencies(t, exchangeRateResponse.BaseCurrency, supportedCurrencyCodes, exchangeRateResponse.ExchangeRates)

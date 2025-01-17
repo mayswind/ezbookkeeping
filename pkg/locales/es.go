eba9a59336b51b244666d12fb52e1bdc0d7fdd8e
MiGueL0n <54883648+Miguelonlonlon@users.noreply.github.com>
2025-01-17 05:05:38 +0100

    Backend spanish language
    Sorry is my first pull 🤦‍♂️ i forgot to add Spanish in backend structure.

---
+++ pkg/locales/es.go
@@ -0,0 +1,30 @@
+package locales
+
+import (
+	"github.com/mayswind/ezbookkeeping/pkg/core"
+)
+
+var en = &LocaleTextItems{
+	DefaultTypes: &DefaultTypes{
+		DecimalSeparator:    core.DECIMAL_SEPARATOR_DOT,
+		DigitGroupingSymbol: core.DIGIT_GROUPING_SYMBOL_COMMA,
+	},
+	DataConverterTextItems: &DataConverterTextItems{
+		Alipay:       "Alipay",
+		WeChatWallet: "Wallet",
+	},
+	VerifyEmailTextItems: &VerifyEmailTextItems{
+		Title:                     "Verifique su cuenta de correo",
+		SalutationFormat:          "Hola %s,",
+		DescriptionAboveBtn:       "Por favor, haga click en el siguiente enlace para confirmar su cuenta de correo.",
+		VerifyEmail:               "Verificar correo",
+		DescriptionBelowBtnFormat: "Si no registró una cuenta de %s, simplemente haga caso omiso a este correo. Si no puede hacer click en el link de verificación, copie la url arriba mostrada y péguela en su navegador. El enlace de verificación de correo expira pasados %v minutos.",
+	},
+	ForgetPasswordMailTextItems: &ForgetPasswordMailTextItems{
+		Title:                     "Restablezca su Contraseña",
+		SalutationFormat:          "Hola %s,",
+		DescriptionAboveBtn:       "Hemos recibido una solicitud para restablecer su contraseña. Puede hacer click en el siguiente link para restablecer su contraseña.",
+		ResetPassword:             "Restablecer Contraseña",
+		DescriptionBelowBtnFormat: "Si no solicitó un restablecimiento de contraseña, simplemente descarte este correo. Si no puede hacer click en el link anterior, copie la url arriba mostrada y péguela en su navegadror. El enlace de restablecimiento de contraseña expira pasados %v minutos.",
+	},
+}

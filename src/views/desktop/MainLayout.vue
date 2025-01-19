019c99331371d351dd300310e2dc588e422b34bc
MaysWind <i@mayswind.net>
2025-01-19 21:06:26 +0800

    code refactor

--- src/views/desktop/MainLayout.vue
+++ src/views/desktop/MainLayout.vue
@@ -196,8 +196,7 @@ import SnackBar from '@/components/desktop/SnackBar.vue';
 
 import { ref, computed, useTemplateRef } from 'vue';
 
-import { useDisplay } from 'vuetify';
-import { useTheme } from 'vuetify';
+import { useDisplay, useTheme } from 'vuetify';
 import { useRoute, useRouter } from 'vue-router';
 
 import { useI18n } from '@/locales/helpers.ts';

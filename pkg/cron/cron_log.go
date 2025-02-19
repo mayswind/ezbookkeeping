cf370c083be47683983abdafe65c967c8d48ecbb
MaysWind <i@mayswind.net>
2025-02-20 00:09:12 +0800

    add format string for log content

--- pkg/cron/cron_log.go
+++ pkg/cron/cron_log.go
@@ -15,22 +15,22 @@ type GocronLoggerAdapter struct {
 
 // Debug logs debug log
 func (logger GocronLoggerAdapter) Debug(msg string, args ...any) {
-	log.Debugf(core.NewNullContext(), logger.getFinalLog(msg, args...))
+	log.Debugf(core.NewNullContext(), "%s", logger.getFinalLog(msg, args...))
 }
 
 // Info logs info log
 func (logger GocronLoggerAdapter) Info(msg string, args ...any) {
-	log.Infof(core.NewNullContext(), logger.getFinalLog(msg, args...))
+	log.Infof(core.NewNullContext(), "%s", logger.getFinalLog(msg, args...))
 }
 
 // Warn logs warn log
 func (logger GocronLoggerAdapter) Warn(msg string, args ...any) {
-	log.Warnf(core.NewNullContext(), logger.getFinalLog(msg, args...))
+	log.Warnf(core.NewNullContext(), "%s", logger.getFinalLog(msg, args...))
 }
 
 // Error logs error log
 func (logger GocronLoggerAdapter) Error(msg string, args ...any) {
-	log.Errorf(core.NewNullContext(), logger.getFinalLog(msg, args...))
+	log.Errorf(core.NewNullContext(), "%s", logger.getFinalLog(msg, args...))
 }
 
 func (logger GocronLoggerAdapter) getFinalLog(msg string, args ...any) string {

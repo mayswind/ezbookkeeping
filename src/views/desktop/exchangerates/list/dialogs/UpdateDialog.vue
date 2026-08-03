bcb24b6460849c244c93f8cd74df15b17f4bfec0
MaysWind <i@mayswind.net>
2026-08-04 01:16:45 +0800

    use the new dialog style on the desktop version

--- src/views/desktop/exchangerates/list/dialogs/UpdateDialog.vue
+++ src/views/desktop/exchangerates/list/dialogs/UpdateDialog.vue
@@ -1,63 +1,62 @@
 <template>
     <v-dialog width="800" :persistent="submitting || (!!defaultCurrencyAmount && defaultCurrencyAmount !== 1) || currency !== defaultCurrency || (!!targetCurrencyAmount && targetCurrencyAmount !== 1)" v-model="showState">
-        <v-card class="pa-sm-1 pa-md-2">
-            <template #title>
-                <div class="d-flex align-center">
-                    <h4 class="text-h4">{{ tt('Update User Custom Exchange Rate') }}</h4>
-                </div>
+        <one-column-dialog-layout :disabled="submitting"
+                                  :title="tt('Update User Custom Exchange Rate')" :cancel-button-title="tt('Cancel')"
+                                  @cancel="cancel">
+            <template #toolbar>
+                <v-btn class="mx-2" density="comfortable" variant="outlined"
+                       :disabled="submitting || !defaultCurrencyAmount || !currency || !targetCurrencyAmount"
+                       @click="confirm">
+                    {{ tt('OK') }}
+                    <v-progress-circular indeterminate size="22" class="ms-2" v-if="submitting"></v-progress-circular>
+                </v-btn>
             </template>
-            <v-card-text class="d-flex flex-column flex-md-row mt-2">
-                <v-row>
-                    <v-col cols="12" md="6">
-                        <number-input :autofocus="true"
-                                      :disabled="submitting"
-                                      :label="tt('Amount')"
-                                      :placeholder="tt('Amount')"
-                                      :persistent-placeholder="true"
-                                      :min-value="USER_CUSTOM_EXCHANGE_RATE_MIN_VALUE"
-                                      :max-value="USER_CUSTOM_EXCHANGE_RATE_MAX_VALUE"
-                                      :max-decimal-count="4"
-                                      v-model="defaultCurrencyAmount"
-                                      @keyup.enter="targetAmountInput?.focus()" />
-                    </v-col>
-                    <v-col cols="12" md="6">
-                        <currency-select :disabled="true"
-                                         :label="tt('Currency')"
-                                         :placeholder="tt('Currency')"
-                                         v-model="defaultCurrency" />
-                    </v-col>
-                    <v-col cols="12" class="text-center">
-                        <v-icon :icon="mdiSwapVertical" size="24" />
-                    </v-col>
-                    <v-col cols="12" md="6">
-                        <number-input ref="targetAmountInput" :disabled="submitting"
-                                      :label="tt('Amount')"
-                                      :placeholder="tt('Amount')"
-                                      :persistent-placeholder="true"
-                                      :min-value="USER_CUSTOM_EXCHANGE_RATE_MIN_VALUE"
-                                      :max-value="USER_CUSTOM_EXCHANGE_RATE_MAX_VALUE"
-                                      :max-decimal-count="4"
-                                      v-model="targetCurrencyAmount"
-                                      @keyup.enter="confirm" />
-                    </v-col>
-                    <v-col cols="12" md="6">
-                        <currency-select :disabled="submitting"
-                                         :label="tt('Currency')"
-                                         :placeholder="tt('Currency')"
-                                         v-model="currency" />
-                    </v-col>
-                </v-row>
-            </v-card-text>
-            <v-card-text>
-                <div class="w-100 d-flex justify-center flex-wrap mt-sm-1 mt-md-2 gap-4">
-                    <v-btn :disabled="submitting || !defaultCurrencyAmount || !currency || !targetCurrencyAmount" @click="confirm">
-                        {{ tt('OK') }}
-                        <v-progress-circular indeterminate size="22" class="ms-2" v-if="submitting"></v-progress-circular>
-                    </v-btn>
-                    <v-btn color="secondary" variant="tonal" :disabled="submitting" @click="cancel">{{ tt('Cancel') }}</v-btn>
-                </div>
-            </v-card-text>
-        </v-card>
+
+            <template #content>
+                <v-form class="mt-5">
+                    <v-row>
+                        <v-col cols="12" md="6">
+                            <number-input :autofocus="true"
+                                          :disabled="submitting"
+                                          :label="tt('Amount')"
+                                          :placeholder="tt('Amount')"
+                                          :persistent-placeholder="true"
+                                          :min-value="USER_CUSTOM_EXCHANGE_RATE_MIN_VALUE"
+                                          :max-value="USER_CUSTOM_EXCHANGE_RATE_MAX_VALUE"
+                                          :max-decimal-count="4"
+                                          v-model="defaultCurrencyAmount"
+                                          @keyup.enter="targetAmountInput?.focus()" />
+                        </v-col>
+                        <v-col cols="12" md="6">
+                            <currency-select :disabled="true"
+                                             :label="tt('Currency')"
+                                             :placeholder="tt('Currency')"
+                                             v-model="defaultCurrency" />
+                        </v-col>
+                        <v-col cols="12" class="text-center">
+                            <v-icon :icon="mdiSwapVertical" size="24" />
+                        </v-col>
+                        <v-col cols="12" md="6">
+                            <number-input ref="targetAmountInput" :disabled="submitting"
+                                          :label="tt('Amount')"
+                                          :placeholder="tt('Amount')"
+                                          :persistent-placeholder="true"
+                                          :min-value="USER_CUSTOM_EXCHANGE_RATE_MIN_VALUE"
+                                          :max-value="USER_CUSTOM_EXCHANGE_RATE_MAX_VALUE"
+                                          :max-decimal-count="4"
+                                          v-model="targetCurrencyAmount"
+                                          @keyup.enter="confirm" />
+                        </v-col>
+                        <v-col cols="12" md="6">
+                            <currency-select :disabled="submitting"
+                                             :label="tt('Currency')"
+                                             :placeholder="tt('Currency')"
+                                             v-model="currency" />
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </template>
+        </one-column-dialog-layout>
     </v-dialog>
 
     <snack-bar ref="snackbar" />

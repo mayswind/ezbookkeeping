93dd3ff75a3be72fe31a9e1065975d968405ba3f
Abishekkumar <97280327+abishekkumars@users.noreply.github.com>
2026-09-01 20:47:28 +0530

    Update commit hash retrieval method in vite config
    Replaced git commit hash retrieval with environment variables for build commit hash.

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 import { VitePWA } from 'vite-plugin-pwa';
 import Checker from 'vite-plugin-checker';
-import git from 'git-rev-sync';
-
 import packageFile from './package.json';
 import contributorsFile from './contributors.json';
 import thirdPartyLicenseFile from './third-party-dependencies.json';
@@ -75,7 +73,9 @@ export default defineConfig(() => {
             __EZBOOKKEEPING_IS_PRODUCTION__: process.env['NODE_ENV'] === 'production',
             __EZBOOKKEEPING_VERSION__: JSON.stringify(packageFile.version),
             __EZBOOKKEEPING_BUILD_UNIX_TIME__: JSON.stringify(buildUnixTime),
-            __EZBOOKKEEPING_BUILD_COMMIT_HASH__: JSON.stringify(git.short()),
+            __EZBOOKKEEPING_BUILD_COMMIT_HASH__: JSON.stringify(
+                process.env['RENDER_GIT_COMMIT'] || process.env['BUILD_COMMIT_HASH'] || ''
+            ),
             __EZBOOKKEEPING_CONTRIBUTORS__: JSON.stringify(contributorsFile),
             __EZBOOKKEEPING_LICENSE__: JSON.stringify(licenseContent),
             __EZBOOKKEEPING_THIRD_PARTY_LICENSES__: JSON.stringify(thirdPartyLicenseFile)
